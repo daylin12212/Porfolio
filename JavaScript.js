78c9ce086a88eb51a2c8b53139aec82b49e12db0
daylin12212 <carolinagomezvelez753@gmail.com>
2025-06-02 20:25:44 +0200

    e

--- JavaScript.js
+++ JavaScript.js
@@ -1,11 +1,1 @@
-botonDespliegue.addEventListener('click', () => {
-    let contenido = document.getElementById('contenido');
-    if (contenido.style.display === 'none' || contenido.style.display === '') {
-        contenido.style.display = 'block';
-        botonDespliegue.textContent = 'Ocultar contenido';
-    } else {
-        contenido.style.display = 'none';
-        botonDespliegue.textContent = 'Mostrar contenido';
-    }
 
-});
